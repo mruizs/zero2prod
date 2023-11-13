9c6de4a75d36c656999238a92680288026e6df86
msantiago <9597452+mruizs@users.noreply.github.com>
2023-11-13 13:48:16 +0100

    Async wewritten

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 
-#[get("/")]
-async fn index() -> impl Responder {
-    "Hello, World!"
-}
-
-#[get("/{name}")]
-async fn hello(name: web::Path<String>) -> impl Responder {
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
 }
 
-#[actix_web::main]
+#[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index).service(hello))
-        .bind(("127.0.0.1", 8000))?
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+        .bind("127.0.0.1:8000")?
         .run()
         .await
-}
+    }
